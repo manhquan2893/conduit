14b011235f6f4f86b52c5f41edc265c61584a1cd
manhquan <manhquan2893@gmail.com>
2019-11-06 09:47:57 +0700

    login register

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
-
+import router from './router'
+import store from './store'
+import {CHECK_AUTH} from './store/action.type'
 Vue.config.productionTip = false
 
+router.beforeEach((to, from, next) => {
+  store.dispatch(CHECK_AUTH).then(()=>{next()})
+})
 new Vue({
+  router,
+  store,
   render: h => h(App),
 }).$mount('#app')
